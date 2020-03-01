d1b7a78013e380ae14a746a78d024aceecb42f16
Ankit Solanki <ankit03june@gmail.com>
2020-03-01 16:02:11 +0530

    this is so stupid why can;t i figure out where the help loop is stuck in

--- chord/dht.py
+++ chord/dht.py
@@ -4,45 +4,61 @@ from chord import Chord
 from node import Node
 
 
-# def get_hash(string, size=None):
-# 	if size:
-# 		return hashlib.md5(string.encode()).hexdigest()[:size]
-# 	return hashlib.md5(string.encode()).hexdigest()
+class IntegerTest:
+	def __init__(self, m, _range=1000):
+		self.m = m
+		self.range = _range
 
-# def get_mod(key, m):
-# 	return hex((int('0x'+key, 16)) % (2**m))
+	def setup(self, num):
+		self.chord = Chord(self.m)
 
-m = 16
+		for i in range(10):
+			__id = random.randrange(0, self.range)
+			print("[#] Adding node: ", __id)
+			self.chord.join(Node(__id, m))
+
+		self.chord.fix_fingers()
+
+	def print(self):
+		self.chord.print()
+
+	def insert(self, num, lookup=False):
+		keys = []
+		for i in range(num):
+			print("[.] Inserting file: `file{}`".format(i))
+			key = random.randint(0, self.range)
+			self.chord.insert(key, "`file{}`".format(i))
+			keys.append(key)
+
+
+		if lookup:
+			for i in range(num):
+				val = chord.lookup(keys[i], True)
+				print("Lookup of: {} -> got: {}".format(keys[i], val))
+
+	def self.deletion(self):
+		pass
+
+
+def 
+
+
+def get_hash(string, size=None):
+	if size:
+		return hashlib.md5(string.encode()).hexdigest()[:int(size)]
+	return hashlib.md5(string.encode()).hexdigest()
+
+m = 32
 chord = Chord(m)
-
-for i in range(100):
-	r = random.randint(0, 60240) % (2**m)
-# 	hesh = get_hash(str(r), 8)
+	
+for i in range(10):
+	r = get_hash(str(random.randrange(0, 102400)), size=(m/4))
 	print("[*] Adding node | id -> {}".format(r))
 	res = chord.join(Node(r, m))
-# 	# print("res ", res)
-# 	# print("i ", i)
+	print("res ", res)
 
 
 # print("coming here")
 chord.fix_fingers()
-# keys = []
-# for i in range(1, 5):
-# 	print("[.] Inserting file: file{}".format(i))
-# 	key = random.randint(0, 2**10)
-# 	chord.insert(key, "file{}".format(i))
-# 	keys.append(key)
-# print("coming here too")
-
 chord.print()
-# l = int(input())
-
-# chord.leave_node_by_key(l)
-# chord.fix_fingers()
-# chord.print()
-# input()
-
-# for i in range(1, 5):
-# 	val = chord.lookup(keys[i-1], True)
-# 	print("\n{}->{}".format(keys[i-1], val))
 
